08cad0a8a492f92dfbca40ae3aa0eeed66ddb0ef
tauhid234 <syifatauhid@gmail.com>
2022-01-26 22:29:24 +0700

    ROUTE USER

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -7,8 +7,8 @@ const auth = new AuthController;
 
 const router = Router();
 
-router.get("/user", auth.getAuth, user.getUser);
-router.post("/user/add", user.createUser);
+router.get("/api/user", auth.getAuth, user.getUser);
+router.post("/api/user/add", user.createUser);
 // router.post("/user/:id", );
 // router.post("/user/delete/:id", );
 
